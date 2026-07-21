435ae4977409616892e8cf33bc0896b3b35826fb
gilart729157 <claudianoart@gmail.com>
2026-07-21 19:03:24 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,41 @@
+/* ==========================================
+   CONFIGURAÇÕES DA RIFA
+========================================== */
+
+const CONFIG = {
+
+    // Informações principais
+    titulo: "Rifa Beneficente",
+
+    beneficiada: "Benedita Laranjeiras dos Santos",
+
+    premio: "Geladeira Midea Frost Free",
+
+    valorNumero: 10,
+
+    dataSorteio: "30/12/2026",
+
+    resultado:
+    "1ª Premiação da Loteria Federal",
+
+
+    // Números da rifa
+    quantidadeNumeros: 1000,
+
+    numerosPorCartela: 100,
+
+
+    // Pagamento
+    pixChave:
+    "588.472.355-53",
+
+
+    // Contato
+    whatsapp:
+    "5579999145044",
+
+
+    // Firebase
+    caminhoReservas:
+    "reservas",
+
