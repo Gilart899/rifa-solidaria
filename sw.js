992892e998dd4a2075293fdd701721fe5fd7f89a
gilart729157 <claudianoart@gmail.com>
2026-08-10 12:51:36 -0300

    Add service worker caching for static assets

--- sw.js
+++ sw.js
@@ -1,1 +1,2 @@
 
+const C="rifa-gilfest-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","./cartela.html","./style.css"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
